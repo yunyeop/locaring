1b3fcb39565252188ec03ccc21676a07023c8f92
yunyeop <yykim000922@gmail.com>
2022-02-07 01:11:28 +0900

    feat:naver-map resolved crack (in ios device)

--- app/ios/Pods/Target Support Files/Pods-locaring/Pods-locaring-frameworks.sh
+++ app/ios/Pods/Target Support Files/Pods-locaring/Pods-locaring-frameworks.sh
@@ -176,10 +176,12 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${PODS_ROOT}/NMapsMap/framework/NMapsMap.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/Flipper-DoubleConversion/double-conversion.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/OpenSSL-Universal/OpenSSL.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${PODS_ROOT}/NMapsMap/framework/NMapsMap.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/OpenSSL-Universal/OpenSSL.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
